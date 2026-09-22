4ee7d6faac39013c57bf6a28dfc1fd5352104e59
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-09-22 17:34:57 +0530

    New version is ready

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('', include('accounts.urls')), # Mount accounts at root
-    path('projects/', include('projects.urls')),
+    path('projects_explorer/', include('projects.urls')),
 ]
 
 handler403 = 'accounts.views.custom_403_view'
